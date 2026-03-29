70e048858a14ac18fda69eca295ae27fb3e86031
frontendab <ayoubpourletravail@gmail.com>
2026-03-29 23:19:28 +0100

    Store name and covert image of the country, and start working on store the visa requirements

--- app.js
+++ app.js
@@ -1,55 +1,85 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require("puppeteer-extra-plugin-stealth")
+const cheerio = require("cheerio")
 
-// async function playwrightTest() {
-//   // let obj = {}
-//   // let arr = []
-
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   await page.goto("https://www.passportindex.org");
-  
-//   // const html = await page.content()
-
-//   // const $ = cheerio.load(html)
-
-//   // async function get_passports() {
-//   //   const elements = await page.locator('.passimg img');
-    
-//   //   await elements.first().click()
-
-//   //   const center_block = await page.locator(".center-block td .psprt-view-dashboard")
-//   //   const link = await center_block.first().getAttribute("href")
-
-//   //   await page.goto(link, {waitUntil: "networkidle"})
-
-//   //   await page.waitForTimeout(Math.floor(Math.random() * 4000 + 1000));
-
-//   //   // const count = await elements.count();
-    
-//   //   // for (let i = 0; i < count; i++) {
-//   //   //   await elements.nth(i).click();
-
-//   //   //   const elements = page.locator('.passimg img');
-//   //   // }
-//   // }
-
-//   // await get_passports()
-
-//   // await page.waitForTimeout(2000)`
-
-//   await browser.close();
-// }
-
-// playwrightTest();
-
-
-const puppeteer = require('puppeteer');
+puppeteer.use(StealthPlugin());
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.freecodecamp.org/');
+  let obj = {}
+  let passports_info = []
 
-    await browser.close();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--window-size=1920,1080',
+      '--disable-blink-features=AutomationControlled' // Key for UC mode
+    ]
+  });
+  const page = await browser.newPage();
+  const html = await page.content();
+  const get = cheerio.load(html)
+
+  const cdp = await page.target().createCDPSession();
+
+  // Example: Use CDP to bypass touch/platform detection
+  await cdp.send('Page.addScriptToEvaluateOnNewDocument', {
+    source: `
+      Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+    `
+  });
+
+  await page.goto("https://www.passportindex.org", { waitUntil: "networkidle0" });
+
+  const divElements = await page.$$("div.passimg");
+
+  if (divElements.length > 0) {
+
+    await divElements[0].click();
+
+    try {
+      const name = await page.$eval('#psprt-cname', title => title.innerText);
+      const cover_src = await page.$eval("#psprt-cover", img => img.src);
+
+      obj["name_passport"] = name.trim();
+      obj["cover"] = cover_src;
+
+      if (obj.name_passport && obj.cover) {
+        const link = await page.$eval("a.psprt-view-dashboard", link => link.href);
+        await page.goto(link, { waitUntil: "networkidle0" })
+
+        const html = await page.content();
+        const get = cheerio.load(html)
+
+        // TODO: I have to store visa requirements correctly
+        const visa_requirements = await get("table.psprt-dashboard-table tbody .show-tr")
+        .each((i, el) => {
+          const visa_requirements = []
+          const flag = get(el).find("span");
+          const country_name = get(el).find("a").text().trim();
+
+          visa_requirements.push({
+            flag, country_name
+          });
+          return visa_requirements
+        });
+
+        obj["visa_requirements"] = visa_requirements
+
+      } else {
+        console.log("NAME AND COVER_SRC IS NOT FULL!");
+      }
+    } catch (error) {
+      console.log("Catch: Render again...");
+    }
+
+    passports_info.push(obj)
+  }
+
+  console.log(passports_info);
+
+  await new Promise(r => setTimeout(r, 5000))
+
+  await browser.close();
 })();
